9015116c04cf16b3a50f7fa9ffc4f5ae2e570b5f
unknown <photogenicbug@gmail.com>
2020-05-31 22:25:20 +0530

    fixed multiple face detection

--- facedetect_raspberry.py
+++ facedetect_raspberry.py
@@ -1,12 +1,9 @@
 import cv2
 import numpy as np
-import imutils
 
 def detectface(img,model):
 
-    img = imutils.resize(img, width=750)
-
-    (h,w) = img.shape[:2]
+     (h,w) = img.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(img,(300,300)),scalefactor=1.0,size=(300,300),
                                 mean=(104.0,177.0,123.0))
 
@@ -19,16 +16,20 @@ def detectface(img,model):
         if confidence > 0.5:
             box = detections[0,0,i,3:7] * np.array([w,h,w,h])
             (startX, startY, endX, endY) = box.astype("int")
-    
-    face = img[startY:endY,startX:endX]
-    coordinates = [startX, startY, endX, endY]
+            
+            face = img[startY:endY,startX:endX]
+            gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            res = recognizeface(gray,rec)
 
-    return coordinates,face
+            cv2.rectangle(img,(startX, startY),(endX,endY),(0,255,0),2)
+            cv2.putText(img,res,(startX, startY+12),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+    
+    return res, img
 
 def recognizeface(face,rec):
     
     id = 0
-    id, sim = rec.predict(gray)
+    id, sim = rec.predict(face)
     if sim < 85:
             if id == 1:
                 id = 'Rahil {:.2f}'.format(sim)
@@ -57,14 +58,13 @@ if __name__ == '__main__':
         _, frame = cap.read()
 
         try:
-            coordinates, face = detectface(frame,model)
-            gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            res, frame = detectface(frame,model)
+            
         except:
             print('No face detected')
             continue
-
-        res = recognizeface(gray,rec)
-
+        
+        cv2.imshow('Face Recognition',frame)
         print(res)
 
     cv2.destroyAllWindows()
